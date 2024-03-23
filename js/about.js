325c0f33519feac0207ad4f7536f082ce7e02cd5
Victoria Fuselier <vic.fuselier@gmail.com>
2024-03-22 23:57:09 -0500

    Worked on about page pop ups, project page
    Project page spacing and layout, responsive. Project page img edit; cropped off white space on side of img (?)
    
    Pop up layout on about page still needs work and I need to program pop up to close when clicking anywhere outside the box

--- js/about.js
+++ js/about.js
@@ -30,59 +30,51 @@ skillsButton.addEventListener('click', () => {
         popUpDiv.classList.remove('hidden');
     }
     
-    popUpDiv.innerHTML =    `<div id='pop-up-box'>
+    popUpDiv.innerHTML =    `<div id="pop-up-box">
                                 <button class="close">x</button>
-                                <ul id="skills">
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                        <ul class="sublist">
-                                            <li>Grid</li>
-                                            <li>Flexbox</li>
-                                            <li>Transforms and Transitions</li>
-                                            <li>Keyframe Animations</li>
-                                            <li>Sass</li>
-                                            <ul>
-                                                <li class="sublist-level-2">and more!</li>
-                                            </ul>                            
-                                        </ul>
-                                    <li>JavaScript</li>
-                                        <ul class="sublist">
-                                            <li>DOM Selectors</li>
-                                            <li>DOM Manipulation</li>
-                                            <li>DOM Traversal</li>
-                                            <li>Callback functions</li>
-                                            <li>Array Methods</li>
-                                            <li>Array Iteration Methods</li>
-                                            <li>Object-Oriented JavaScript</li>
-                                            <li>Object Interactions</li>
-                                            <li>Callback Functions</li>
-                                            <li>Fetch API</li>
-                                                <ul>
-                                                    <li class="sublist-level-2">and more!</li>
+                                <div id="pop-up-content">
+                                    <div id="pop-up-list">
+                                        <ul id="skills">
+                                            <li>HTML</li>
+                                            <li>CSS</li>
+                                                <ul class="sublist">
+                                                    <li>Grid</li>
+                                                    <li>Flexbox</li>
+                                                    <li>Transforms and Transitions</li>
+                                                    <li>Keyframe Animations</li>
+                                                    <li>Sass</li>
+                                                    <ul>
+                                                        <li class="sublist-level-2">and more!</li>
+                                                    </ul>                            
                                                 </ul>
+                                            <li>JavaScript</li>
+                                                <ul class="sublist">
+                                                    <li>DOM Selectors</li>
+                                                    <li>DOM Manipulation</li>
+                                                    <li>DOM Traversal</li>
+                                                    <li>Callback functions</li>
+                                                    <li>Array Methods</li>
+                                                    <li>Array Iteration Methods</li>
+                                                    <li>Object-Oriented JavaScript</li>
+                                                    <li>Object Interactions</li>
+                                                    <li>Callback Functions</li>
+                                                    <li>Fetch API</li>
+                                                        <ul>
+                                                            <li class="sublist-level-2">and more!</li>
+                                                        </ul>
+                                                </ul>
+                                            <li>Responsive Design</li>
+                                            <li>Bootstrap</li>
+                                            <li>Github</li>
+                                            <li>SVG Basics</li>
+                                            <li>JQuery</li>
+                                            <li>AJAX Basics</li>
+                                            <li>Debugging with Chrome DevTools</li>
+                                            <li>Web Design</li>
+                                            <li>Accessibility</li>
                                         </ul>
-                                    <li>Responsive Design</li>
-                                    <li>Bootstrap</li>
-                                    <li>Github</li>
-                                    <li>SVG Basics</li>
-                                    <li>JQuery</li>
-                                    <li>AJAX Basics</li>
-                                    <li>Debugging with Chrome DevTools</li>
-                                    <li>Web Design</li>
-                                    <li>Accessibility</li>
-                                    <li>Familiarity with Resources</li>
-                                    <li>Currently Working On:</li>
-                                        <ul class="sublist">
-                                            <li>Practicing & Solidifying Previously-Listed Skills</li>
-                                            <li>Refactoring & Improving Portfolio Projects</li>
-                                        </ul>
-                                    <li>Want to Learn Next:</li>
-                                        <ul class="sublist">
-                                            <li>C# Basics</li>
-                                            <li>SQL</li>
-                                        </ul>
-                                </ul>
-                                <p>Check out my <a tabindex="0" href="https://teamtreehouse.com/profiles/victoriafuselier" target="_blank">Treehouse Profile</a> to see what I've been up to and what I'm currently learning.</p>
+                                    </div>
+                                </div>
                             </div>`;
 
     let closeButton = document.querySelector('.close');
